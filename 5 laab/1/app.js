62f98af195bce5741dc075fa52830acbadec62df
Cactusjust <star_wars_forever@mail.ru>
2025-06-05 17:57:08 +0300

    Add files via upload

---
+++ 5 laab/1/app.js
@@ -0,0 +1,9 @@
+// Переменные
+let age = "22";
+let name = "Shen Qingqiu";
+
+// Применение переменных
+let output = <span>{name} is {age} years old</span>;
+
+// отрисовка
+ReactDOM.render(output, document.querySelector("#myDiv"));  
